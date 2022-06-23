67b6cf5bf9c012a6cd85083c03b0a5a53e2abad9
Linggar <Linggarp49@gmail.com>
2022-06-23 09:25:34 +0700

    Commit Pertama

---
+++ dist/js/script.js
@@ -0,0 +1,22 @@
+// ini bagian hamburger
+
+const hamburger = document.querySelector('#hamburger')
+const navMenu = document.querySelector('#nav-menu')
+
+hamburger.addEventListener('click', function() {
+hamburger.classList.toggle('hamburger-active')
+navMenu.classList.toggle('hidden')
+})
+
+//navbar fixe
+
+window.onscroll = function(){
+    const header = document.querySelector('header')
+    const fixedNav = header.offsetTop;
+
+    if(window.pageYOffset > fixedNav){
+        header.classList.add('navbar-fix')
+    }else{
+        header.classList.remove('navbar-fix')
+    }
+}
